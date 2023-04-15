e2e121b055f1153aff7a52b7ce6ef0e502c2e71c
0xNahhh <0xNahhh@gmail.com>
2023-04-15 17:18:36 +0900

    add script

---
+++ script/Domainoor.s.sol
@@ -0,0 +1,18 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.13;
+
+import "forge-std/Script.sol";
+import {Domainoor} from "../src/Domainoor.sol";
+
+contract DomainoorScript is Script {
+    function setUp() public {}
+
+    function run() public {
+        uint256 deployerPrivateKey = vm.envUint("PRIVATE_KEY");
+        vm.startBroadcast(deployerPrivateKey);
+
+        Domainoor domainoor = new Domainoor();
+        
+        vm.stopBroadcast();
+    }
+}
