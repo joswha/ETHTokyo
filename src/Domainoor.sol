4b046c7a8fd2759bbe35e15d30e5d917e1b85c9c
Aaron Esau <git@aaronesau.com>
2023-04-15 14:31:43 +0900

    add basic files

---
+++ src/Domainoor.sol
@@ -0,0 +1,57 @@
+pragma soidity ^0.9.0;
+
+import "./iDomainoor.sol";
+import "@openzeppelin/contracts/access/AccessControl.sol";
+import "@openzeppelin/contracts/utils/structs/EnumerableSet.sol";
+
+contract Domainoor is iDomainoor, AccessControl {
+        
+    mapping(bytes32 => DomainObject) public domainStorage;
+    mapping(bytes32 => address) public domainOwner;
+    uint256 public timelock;
+    bytes32 public constant ENDPOINT_ROLE = 0x1337;
+
+    constructor(address _endpoint) {
+        timelock = 1 days;
+        _setupRole(DEFAULT_ADMIN_ROLE, msg.sender);
+        _setupRole(ENDPOINT_ROLE, _endpoint);
+    }
+
+    // ===================SETTERS===================
+    function setDomainOwner(bytes32 memory _domain, address _owner) external override returns (bool) {
+        require(hasRole(ENDPOINT_ROLE, msg.sender), "Domainoor: Not an endpoint");
+        require(_owner != address(0), "Domainoor: Invalid address");
+        // require(_domain.length == 32) ...
+        require(domainOwner[_domain] == address(0), "Domainoor: Domain already registered");
+
+        domainOwner[_domain] = _owner;
+        return true;
+    }
+
+    function updateDomain(bytes32 memory _domain, address[] _contracts, uint256 _state) external override returns (bool) {
+        require(domainOwner[_domain] == msg.sender, "Domainoor: Not the domain owner");
+        require(_state < 3, "unsuported _state mutex");
+
+        // first call
+        if(domainStorage[_domain].creationDate == 0) {
+            domainStorage[_domain].creationDate = block.timestamp;
+        }
+
+        // update then contracts list if necessary
+        for(uint256 i = 0; i < _contracts.length; i++) {
+            require(_contracts[i] != address(0));
+
+            domainStorage[_domain].contracts.push(_contracts[i]);
+        }
+
+        // update the state if necessary
+        domainStorage[_domain].state = _state;
+        return true;
+    }
+
+    // ===================GETTERS===================
+    function getDomainOwner(bytes32 memory _domain) external view override returns (address) {
+        return domainOwner[_domain];
+    }
+
+}
---
+++ src/iDomainoor.sol
@@ -0,0 +1,16 @@
+pragma solidity ^0.9.0;
+
+interface iDomainoor {
+
+    struct DomainObject {
+        address[] contracts,
+        uint256 state, // 0 - Unregistered, 1 - Registered, 2 - Suspended, 3 - Deleted
+        uint256 creationDate
+    }
+     
+    function setDomainOwner(bytes32 memory _domain, address _owner) external returns (bool);
+    function updateDomain(bytes32 memory _domain, DomainObject _domainDetails) external returns (bool);
+    function getDomainOwner(bytes32 memory _domain) external view returns (address);
+    // function setTimelock(uint256 _timelock) external returns (bool);
+    // function getTimelock() external view returns (uint256);
+}
